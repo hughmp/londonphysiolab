20b46a96b20e213e7740d520b44e61df2812b9e2
hughmp <hughmp@users.noreply.github.com>
2017-01-19 19:17:59 +0000

    header updates

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './bootstrap.min.css';
 import './App.css';
 
-import { Navbar, NavItem, Nav, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 import logo from '../public/images/london-physio-lab-logo.png';
 
@@ -11,23 +11,22 @@ const App = () => (
     <Navbar inverse fixedTop collapseOnSelect>
       <Navbar.Header>
         <Navbar.Brand>
-          <img src={logo} alt="London Physio Lab" className="logo"/>
+          <img src={logo} alt="London Physio Lab" className="logo" />
         </Navbar.Brand>
         <Navbar.Toggle />
       </Navbar.Header>
       <Navbar.Collapse>
         <Nav pullRight>
-          <NavItem eventKey={1} href="#">Link</NavItem>
-          <NavItem eventKey={2} href="#">Link</NavItem>
-          <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-            <MenuItem eventKey={3.1}>Action</MenuItem>
-            <MenuItem eventKey={3.2}>Another action</MenuItem>
-            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+          <NavItem eventKey={1} href="#">About</NavItem>
+          <NavDropdown eventKey={2} title="Services" id="basic-nav-dropdown">
+            <MenuItem eventKey={2.1}>Pain</MenuItem>
+            <MenuItem eventKey={2.2}>Muscular</MenuItem>
+            <MenuItem eventKey={2.3}>Sports</MenuItem>
             <MenuItem divider />
-            <MenuItem eventKey={3.3}>Separated link</MenuItem>
+            <MenuItem eventKey={2.3}>Clinics</MenuItem>
           </NavDropdown>
-          <NavItem eventKey={4} href="#">Link Right</NavItem>
-          <NavItem eventKey={5} href="#">Link Right</NavItem>
+          <NavItem eventKey={4} href="#">Location</NavItem>
+          <NavItem eventKey={5} href="#">Contact</NavItem>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
