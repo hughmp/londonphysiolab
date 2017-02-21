2496f895b9ec7287149ea91a177ae321a7915060
hughmp <hughmp@users.noreply.github.com>
2017-02-21 23:19:19 +0000

    adding home text

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -11,23 +11,21 @@ export default () => (
       <h1>your wellbeing = my focus</h1>
     </div>
     <div className='text'>
-      <p>
-        <h1>Why me?</h1>
-        <br />
-        <ul>
-          <li>Providing experienced chartered Physiotherapy, over 10 years experience</li>
-          <li>Support you on your road to recovery</li>
-          <li>Open and clear communication throughout</li>
-          <li>Give you the knowledge you need to properly manage your condition</li>
-          <li>Get to the cause of your symptoms to ensure it doesn't return</li>
-          <li>Gold standard 1 hour initial consultation</li>
-          <li>Work with the best specialists, surgeons and other top notch professionals, if no change in 3 sessions you can be referred to the right specialist for you for further investigations</li>
-          <li>Individual treatment and rehabilitation programs designed specifically for you, with your aims in mind</li>
-          <li>Special interest in feet and all overuse running injuries</li>
-          <li>Special interest in chronic centralised pain, low back and neck pain</li>
-          <li>Special interest in shoulder, hip and knee rehabilitation</li>
-        </ul>
-      </p>
+      <h1>Why me?</h1>
+      <br />
+      <ul>
+        <li><i className="fa fa-check" aria-hidden="true"></i> Providing experienced chartered Physiotherapy, over 10 years experience</li>
+        <li><i className="fa fa-check" aria-hidden="true"></i> Support you on your road to recovery</li>
+        <li><i className="fa fa-check" aria-hidden="true"></i> Open and clear communication throughout</li>
+        <li><i className="fa fa-check" aria-hidden="true"></i> Give you the knowledge you need to properly manage your condition</li>
+        <li><i className="fa fa-check" aria-hidden="true"></i> Get to the cause of your symptoms to ensure it doesn't return</li>
+        <li><i className="fa fa-check" aria-hidden="true"></i> Gold standard 1 hour initial consultation</li>
+        <li><i className="fa fa-check" aria-hidden="true"></i> Work with the best specialists, surgeons and other top notch professionals, if no change in 3 sessions you can be referred to the right specialist for you for further investigations</li>
+        <li><i className="fa fa-check" aria-hidden="true"></i> Individual treatment and rehabilitation programs designed specifically for you, with your aims in mind</li>
+        <li><i className="fa fa-check" aria-hidden="true"></i> Special interest in feet and all overuse running injuries</li>
+        <li><i className="fa fa-check" aria-hidden="true"></i> Special interest in chronic centralised pain, low back and neck pain</li>
+        <li><i className="fa fa-check" aria-hidden="true"></i> Special interest in shoulder, hip and knee rehabilitation</li>
+      </ul>
     </div>
   </div>
 );
