d9aa47db42e57604722a2294837e465e548ef106
hughmp <hughmp@users.noreply.github.com>
2017-02-25 17:58:51 +0000

    temp contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import { Link } from 'react-router';
+
 export default () => (
   <div className="content-container">
-    <span className="text-center"> Contact </span>
-  </div>
+    <span className="text-center">
+      <pre>Please see <Link to='/locations'>locations</Link> in the menu ^</pre>
+    </span>
+  </div >
 );
